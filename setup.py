661c9138d5e0b0d22cf0d8fa8b71bad676d28927
Michał Wawrzynowicz <michalwa2003@gmail.com>
2020-07-28 20:10:32 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ratelimit',
+    version='1.0',
+    license='MIT',
+    description='Rate limiting for function calls',
+    author='michalwa',
+    author_email='michalwa2003@gmail.com',
+    url='https://github.com/michalwa/py-ratelimit',
+    packages=find_packages()
+)
